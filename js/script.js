42372ffbba9f75d4de5c3c18cf6967dadaaa8fa0
Olga <olga.vanil@gmail.com>
2020-06-28 21:39:57 +0300

    First test commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = prompt("Вам есть 18?", "18");
+console.log(typeof(answer)); */
+
+const answers = [];
+
+/* answers[0] = prompt('Ваше имя', '');
+answers[1] = prompt('Ваша фамилия', '');
+answers[2] = prompt('Ваш возраст', ''); */
+
+console.log(typeof(1));
+
+
+const user = 'Ольга';
+console.log(`Привет, ${user}`);
