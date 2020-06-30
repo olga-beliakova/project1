b21dcc3c8becf7028afed64b28d294bc59a4b522
Olga <olga.vanil@gmail.com>
2020-06-30 12:57:05 +0300

    Практическое задание 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-let number = 5;
+/* let number = 5;
 const leftBorderWidth = 1;
 
 number = 10;
-console.log(number);
+console.log(number); */
 
 /* const result = confirm("Are you here?");
 console.log(result); */
@@ -12,14 +12,39 @@ console.log(result); */
 /* const answer = prompt("Вам есть 18?", "18");
 console.log(typeof(answer)); */
 
-const answers = [];
+/* const answers = []; */
 
 /* answers[0] = prompt('Ваше имя', '');
 answers[1] = prompt('Ваша фамилия', '');
 answers[2] = prompt('Ваш возраст', ''); */
 
-console.log(typeof(1));
-
+/* console.log(typeof(1));
 
 const user = 'Ольга';
-console.log(`Привет, ${user}`);
+console.log(`Привет, ${user}`); */
+
+let numberOfFilms;
+numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let answerMovie,
+    answerRating;
+
+answerMovie = prompt('Один из последних просмотренных фильмов?','');
+answerRating = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[answerMovie] = answerRating;
+
+answerMovie = prompt('Один из последних просмотренных фильмов?','');
+answerRating = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[answerMovie] = answerRating;
+
+console.log(personalMovieDB);
