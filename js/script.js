9f36ab826371853df34a9fef4d5f41da473ffd51
belyakova <belyakova@krista.ru>
2020-07-02 17:52:36 +0300

    Практическое задание 2

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ answers[2] = prompt('Ваш возраст', ''); */
 const user = 'Ольга';
 console.log(`Привет, ${user}`); */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,17 +33,58 @@ const personalMovieDB = {
     privat: false
 };
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
 let answerMovie,
-    answerRating;
+    answerRating,
+    i = 0;
 
-answerMovie = prompt('Один из последних просмотренных фильмов?','');
-answerRating = prompt('На сколько оцените его?','');
+for (let i = 0; i >= 0; i++) {
+    answerMovie = prompt('Один из последних просмотренных фильмов?', '');
+    if (answerMovie != "" && answerMovie != null && answerMovie.length <= 50) {
+        i++;
+    } else {
+        alert("Вы ввели некорректное название фильма");
+        continue;
+    }
+    answerRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[answerMovie] = answerRating;
+    if (i > 1) {
+        break;
+    }
+}
 
-personalMovieDB.movies[answerMovie] = answerRating;
+/*while (i < 2) {
+    answerMovie = prompt('Один из последних просмотренных фильмов?', '');
+    if (answerMovie != "" && answerMovie != null && answerMovie.length <= 50) {
+        i++;
+    } else {
+        alert("Вы ввели некорректное название фильма");
+        continue;
+    }
+    answerRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[answerMovie] = answerRating;
+}
 
-answerMovie = prompt('Один из последних просмотренных фильмов?','');
-answerRating = prompt('На сколько оцените его?','');
+do {
+    answerMovie = prompt('Один из последних просмотренных фильмов?', '');
+    if (answerMovie != "" && answerMovie != null && answerMovie.length <= 50) {
+        i++;
+    } else {
+        alert("Вы ввели некорректное название фильма");
+        continue;
+    }
+    answerRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[answerMovie] = answerRating;
+} while (i < 2)*/
 
-personalMovieDB.movies[answerMovie] = answerRating;
 
 console.log(personalMovieDB);
